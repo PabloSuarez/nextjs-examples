54d7c415d6c5aa4722acfa7d171618de680e96e4
Pablo Suarez <pablo.suarez@influenster.com>
2021-05-16 19:34:20 -0300

    Fix typo.

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -6,7 +6,7 @@ import styles from '/styles/global.module.css';
 const Footer = () => {
     return (
         <footer className={styles.footer}>
-            <a target="_blank" href="https://github.com/PabloSuarez/nextjs-examples">Check the code source on GitHub</a>
+            <a target="_blank" href="https://github.com/PabloSuarez/nextjs-examples">Check the source code on GitHub</a>
             <a
                 href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
                 target="_blank"
